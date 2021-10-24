7952b2b2e4839e1e05edf8036d2c505c69fe2de7
shane00711 <shane08003@gmail.com>
2021-10-24 14:46:40 +0200

    Some flexbos setup and button routing from landing

--- fronend/src/components/landing/landing.tsx
+++ fronend/src/components/landing/landing.tsx
@@ -1,7 +1,20 @@
+import { useHistory } from 'react-router';
+import './landing.css';
 export const Landing = () => {
+    const history = useHistory();
+
     return (
-        <div>
-            Landing Screen
+        <div className="container">
+            <div className="col">
+                <h1>Welcome to Crazy 8</h1>
+                <h2>The game were the winner takes all.</h2>
+            </div>
+            <div className="col">
+                <button className="login_btn" onClick={()=>history.push('/login')}>Login (Already known)</button>
+            </div>
+             <div className="col">
+                <button className="reg_btn" onClick={()=>history.push('/reg')}>Register (New Player)</button>
+            </div>
         </div>
     )
 }
